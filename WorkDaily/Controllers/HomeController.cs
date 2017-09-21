761b03a43c3997f30cd42df1d05c633e39be6953
Harry.Chan <harry.chan@soohoobook.com>
2017-09-21 11:04:33 +0800

    add mobilehelper for detect user device is mobile or pc

--- WorkDaily/Controllers/HomeController.cs
+++ WorkDaily/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WorkDaily.Models;
 
 namespace WorkDaily.Controllers
 {
@@ -25,6 +26,16 @@ namespace WorkDaily.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Welcome()
+        {
+            //判斷裝置
+            if (!MobileHelper.IsMobileDevice(Request.UserAgent))
+            {
+                return Redirect("https://github.com/DakHarry/WorkDaily");
+            }
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ WorkDaily/Models/MobileHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WorkDaily.Models
+{
+    public class MobileHelper
+    {
+        /*
+         * 手機會出現的userAgent關鍵字
+         */
+        private static string[] mobileDevices = new string[]
+       {
+            "iphone","ppc",
+            "windows ce","blackberry",
+            "opera mini","mobile","palm",
+            "portable","opera mobi"
+       };
+
+        /* 判斷使用者所用的裝置是手機/電腦         
+         --------------------------------*/
+        public static bool IsMobileDevice(string userAgent)
+        {
+            userAgent = userAgent.ToLower();
+            return mobileDevices.Any(x => userAgent.Contains(x));
+        }
+    }
+}
